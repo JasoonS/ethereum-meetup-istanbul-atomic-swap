12da2e981af135d72037d97a1280d42f3a0389d4
Jason Smythe <jason@smythemail.za.net>
2018-10-28 13:30:26 +0300

    Add deployment to different test networks

---
+++ config.templayte.js
@@ -0,0 +1,4 @@
+module.exports = {
+  memonic: 'a b c d e f g',
+  accessToken: '<your infura token>',
+}
--- truffle.js
+++ truffle.js
@@ -1,9 +1,24 @@
+const HDWalletProvider = require("truffle-hdwallet-provider")
+const { memonic, accessToken } = require('./config.js') 
+
 module.exports = {
   networks: {
     development: {
-      host: '127.0.0.1',
+      host: 'localhost',
       port: 8545,
-      network_id: '*',
+      network_id: '*' // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(memonic, "https://rinkeby.infura.io/" + accessToken)
+      },
+      network_id: 3
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(memonic, "https://kovan.infura.io/" + accessToken)
+      },
+      network_id: 3
     }
   }
-}
+};
